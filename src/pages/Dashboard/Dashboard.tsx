29f552c605f9c51bb6b8ffe4e216d6d91b8c15b7
Matheus Dos Santos <matheus2050@gmail.com>
2024-10-14 00:37:29 -0300

    Adicionado titulo na dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,12 @@
 // src/pages/Dashboard/Dashboard.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import DashboardLayout from '../../components/DashboardLayout/DashboardLayout';
 import TodoList from '../../components/TodoList/TodoList';
 
 const Dashboard: React.FC = () => {
+    useEffect(() => {
+        document.title = 'Dashboard';
+      }, []);
     return (
         <DashboardLayout>
             <h2 className="text-2xl font-bold mb-4">Bem-vindo ao painel</h2>
